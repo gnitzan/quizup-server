00117872ebb0dcdd398b64e5f3fae2d3a5f804dd
galn <galn@opisoft.com>
2016-05-31 05:47:33 +0000

    added comment

--- src/main/java/com/rom/quizup/server/QuizupServerApplication.java
+++ src/main/java/com/rom/quizup/server/QuizupServerApplication.java
@@ -1,8 +1,20 @@
 package com.rom.quizup.server;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point to the web based application.
+ * 
+ * When the Spring framework initializes, it loads
+ * the @see {@link Tomcat} servlet container, loads the
+ * configuration files and scan the code base for annotated
+ * classes. Based on the annotations it configures for us
+ * automatically all the APIs, services and repositories.
+ * 
+ * @author rom
+ */
 @SpringBootApplication
 public class QuizupServerApplication {
 
